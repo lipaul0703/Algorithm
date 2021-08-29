e694d4db4e43bfd47f4ec55240c1f6c0ddf63d03
Reset.Lee <Reset.Lee>
2021-08-29 22:01:19 +0700

    v1.0

---
+++ binary serch algorithm.py
@@ -0,0 +1,20 @@
+def binary_search(x, y):
+    x_low = 0
+    x_high = len(x) - 1
+    while x_low <= x_high:
+        x_mid = (x_low+x_high) // 2
+        if x[x_mid] == y:
+            return True, x_mid
+        elif x[x_mid] < y:
+            x_low = x_mid+1
+        elif x[x_mid] > y:
+            x_high = x_mid-1
+        else:
+            return None
+
+
+if __name__ == '__main__':
+    y = 7
+    list = [1, 3, 4, 7, 9, 11, 14, 17]
+    y_index = binary_search(list, y)
+    print(y_index[1])
